cc1e1c289822f21f157e50cad304bda974b3a7d4
RaphaelVR <raphaelventura55@gmail.com>
2024-01-25 11:39:46 +0100

    components

---
+++ src/components/bigCard/BigCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BigCard = () => {
+  return (
+    <div className='app__bigCard'>BigCard</div>
+  )
+}
+
+export default BigCard
---
+++ src/components/card/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='app__card'>Card</div>
+  )
+}
+
+export default Card
