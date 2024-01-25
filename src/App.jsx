05c4217c19ba3c99b16b54d8ba4f493e61497860
RaphaelVR <raphaelventura55@gmail.com>
2024-01-25 13:36:35 +0100

    navbar & scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+// import React from 'react'
+import Navbar from './components/navbar/Navbar'
+import Menu from './components/menu/Menu'
+import Card from './components/card/Card'
+import BigCard from './components/bigCard/BigCard'
+import './styles/global.scss'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app__main'>
+      <div className='app__navbar'>
+        <Navbar />
+      </div>
+        <div className='app__container'>
+          <div className='app__menuContainer'>
+            <Menu />
+          </div>
+          <div className='app__contentContainer'>
+            <div className='app__card'>
+              <Card />
+            </div>
+          </div>          
+        </div>
+        <div className='app__bigCard'>
+            <BigCard />
+        </div>
+
+    </div>
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import "./navbar.scss";
 
 const Navbar = () => {
   return (
-    <div className='app__navbar'>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <div className="logo">
+        <img src="logo.svg" alt="" />
+        <span>Raphael Dashboard</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="" className="icon" />
+        <img src="/app.svg" alt="" className="icon" />
+        <img src="/expand.svg" alt="" className="icon" />
+        <div className="notification">
+          <img src="/notifications.svg" alt="" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img
+            src="RaphaelPro.png"
+            alt=""
+          />
+          <span>Raphael</span>
+        </div>
+        <img src="/settings.svg" alt="" className="icon" />
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
