5b0e6447c736952ed840979b9771dfe06d5f37ac
RaphaelVR <raphaelventura55@gmail.com>
2024-01-25 15:40:07 +0100

    home & cards & scss

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import './Card.scss'
 
 const Card = () => {
   return (
-    <div className='app__card'>Card</div>
+    <div className="main">
+
+      <div className='card card1'>Card1</div>
+      <div className='card card2'>Card2</div>
+      <div className='card card3'>Card3</div>
+      <div className='card card4'>Card4</div>
+      <div className='card card5'>Card5</div>
+      <div className='card card6'>Card6</div>
+      <div className='card card7'>Card7</div>
+
+    </div>
   )
 }
 
