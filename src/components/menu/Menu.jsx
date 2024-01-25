6e0146d72781707334d8de31f6d41a1b9f3706a8
RaphaelVR <raphaelventura55@gmail.com>
2024-01-25 14:17:38 +0100

    menu & scss

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import './Menu.scss'
 
 const Menu = () => {
   return (
-    <div className='app__menu'>Menu</div>
+    <div className='menu'>
+      <div className='item'>
+        <span className='title'>MAIN</span>    
+          <div className='listItem'>
+          
+          <img src="app.svg" alt="" />
+          <span>APP</span>
+
+          </div>
+
+          <div className='listItem'>
+          
+          <img src="app.svg" alt="" />
+          <span>APP</span>
+
+          </div>
+          
+          <div className='listItem'>
+          
+          <img src="app.svg" alt="" />
+          <span>APP</span>
+
+          </div>
+
+          <div className='listItem'>
+          
+          <img src="app.svg" alt="" />
+          <span>APP</span>
+
+          </div>
+
+
+      </div>
+
+    </div>
   )
 }
 
