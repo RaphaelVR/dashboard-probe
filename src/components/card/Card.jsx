8c833b10ad28c4a5eb8005d67799b55e2b25da92
RaphaelVR <raphaelventura55@gmail.com>
2024-01-26 15:40:11 +0100

    AccuWeather API unorganized

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,6 +1,9 @@
 import './Card.scss'
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { AccuWeather } from '../../api/AccuWeather/AccuWeather';
+// import { search } from 'react-icons-kit/feather/search'
+// import { Icon } from 'react-icons-kit'
 
 const Card = () => {
   const url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=5257796615424b19ac78f0e1540fb594";
@@ -15,12 +18,38 @@ const Card = () => {
     fetchInfo();
   }, []);
 
+  const [citySearch, setCitySearch] = useState('');
+  const [cityData, setCityData]=useState(null);
+
+  // city search form
+  const fetchCity = (e) =>{
+    e.preventDefault();
+    axios.get(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=ICfOrVGI3ofdnGODMlLrRMwyPbISOCdO&q=${citySearch}`)
+    .then((res)=>{
+      setCityData(res.data[0]);
+      setCitySearch('');
+    }).catch(err=>console.log(err.message));
+  }
+
   return (
     <div className="main">         
         <div 
         className='card card1'
         >
-        Card1
+          
+            <h1 className="headline">AccuWeather API</h1>
+            <form className='form-group custom-form' autoComplete='off'
+            onSubmit={fetchCity}>
+              <label>Search for a city to get weather data</label>
+              <div className='search-box'>
+                <input className='form-control' required placeholder='Enter city name...'
+                value={citySearch} onChange={(e)=>setCitySearch(e.target.value)}/>
+                <button type='submit' className="btn btn-secondary btn-sm">
+                  {/* <Icon icon={search} size={22}/> */}
+                </button>
+              </div>
+            </form>
+            {cityData&& <div style={{padding:10+'px', width: 100+'%'}}><AccuWeather cityData={cityData}/></div>}
         </div>
         <div 
         className='card card2'
